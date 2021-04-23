a531348db8eea78a11899a72953f567b30ee93ba
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-23 11:19:33 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIO 01 - ESTRUTURA WHILE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EXERCICIO_01___ESTRUTURA_WHILE
+{
+class Program{
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Msg();
+            Conditional();
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg(){
+            Console.WriteLine("Determine qual o quadrante ao qual pertence o ponto.");
+        }
+        //------------------------------------------------------------------------------------------//
+        static void Conditional(){
+            Console.Write("DIGITE A SENHA: ");
+            int password = int.Parse(Console.ReadLine());
+
+            while (password != 2002){
+                Console.Write("SENHA INVALIDA DIGITE NOVAMENTE: ");
+                password = int.Parse(Console.ReadLine());//---------> DEVE-SE POR, PARA NAO CAIR EM LOOP INFINITO.
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+    }
+}
